b5151b077584d8e604bdb03da62ec796678a15f7
NelsonFrancis <email.nelsonfrancis@gmail.com>
2022-12-09 18:20:01 +0530

    Dashboard - user chart and project chart rearranged

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -65,13 +65,13 @@ const Dashboard = () => {
   return (
     <div className='section'>
      <div className='row'>
+     <div className='col-md-4'>
+        <h2 className='dashboard-hd'>Users</h2>
+        <Pie data={users} />
+      </div>
       <div className='col-md-4'>
         <h2 className='dashboard-hd'>Projects</h2>
         <Pie data={projects} />
-      </div>
-      <div className='col-md-4'>
-        <h2 className='dashboard-hd'>Users</h2>
-        <Pie data={users} />
       </div>
       <div className='col-md-4'>
         <h2 className='dashboard-hd'>Tasks</h2>
