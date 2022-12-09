1057cc54c8434ee7a3d9620e74111c84dfb3d74b
NelsonFrancis <email.nelsonfrancis@gmail.com>
2022-12-09 18:18:26 +0530

    3 pie charts added according to projects, users and tasks

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,57 +1,60 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-export const data = {
-  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+export const projects = {
+  labels: ['Ongoing', 'Completed'],
   datasets: [
     {
       label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
+      data: [44, 3],
       backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
         'rgba(54, 162, 235, 1)',
         'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)',
+      ],
+      borderColor: [
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
       ],
       borderWidth: 1,
     },
   ],
 };
 
-export const data2 = {
-  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+export const users = {
+  labels: ['Inactive', 'Active'],
   datasets: [
     {
       label: '# of Votes',
-      data: [10, 10, 10, 10, 10, 10],
+      data: [2, 10],
       backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 99, 132, 1)',
+        'rgba(75, 192, 192, 1)',
       ],
       borderColor: [
         'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
         'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+export const tasks = {
+  labels: ['Completed', 'Pending'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [6, 15],
+      backgroundColor: [
+        "rgba(153, 102, 255, 1)",
+        "rgba(255, 159, 64, 1)"
+      ],
+      borderColor: [
+        "rgba(153, 102, 255, 1)",
+        "rgba(255, 159, 64, 1)"
       ],
       borderWidth: 1,
     },
@@ -59,12 +62,24 @@ export const data2 = {
 };
 
 const Dashboard = () => {
-  const [navActive] = useState(true);
   return (
     <div className='section'>
-     <h1 className='page-hd'>Dashboard</h1>
-     <Pie data={data} />
-     <Pie data={data2} />
+     <div className='row'>
+      <div className='col-md-4'>
+        <h2 className='dashboard-hd'>Projects</h2>
+        <Pie data={projects} />
+      </div>
+      <div className='col-md-4'>
+        <h2 className='dashboard-hd'>Users</h2>
+        <Pie data={users} />
+      </div>
+      <div className='col-md-4'>
+        <h2 className='dashboard-hd'>Tasks</h2>
+        <Pie data={tasks} />
+      </div>
+     </div>
+     
+     
     </div>
   )
 }
